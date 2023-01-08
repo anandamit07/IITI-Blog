cbc4cd1c0a9ae7db1a3f7d0d0d9bb16cbf938443
anandamit07 <amitanand007dav@gmail.com>
2023-01-08 11:50:12 +0530

    added randomstring pkg in auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
+const randomstring = require("randomstring");
 const dotenv = require("dotenv");
 dotenv.config();
 //for send mail
